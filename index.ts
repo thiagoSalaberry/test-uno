f805ec71c1aaeea4f7b758491c6e6cd026225112
Thiago <thiagosalaberry99@gmail.com>
2023-10-02 21:05:39 -0300

    environments vars

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import * as express from "express";
-
 const app = express();
 const port = 2000;
 
 app.use(express.json());
+
+if(process.env.ENVIRONMENT == "develop") {
+    app.get("/env", (req, res)=>{
+        res.json("Estamos en el ambiente de develop")
+    })
+}
 
 app.get("/", (req, res) => {
     res.json("Corriendo gracias al comando start")
